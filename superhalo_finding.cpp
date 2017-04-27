296c1620fb2611799f5e4f428fe58e8944f340d5
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-04-27 10:40:39 -0500

    Add SimHalos, c++11, and ArgParse 0.7.0

--- superhalo_finding.cpp
+++ superhalo_finding.cpp
@@ -6,44 +6,6 @@
 
 #include "libconfig.h++"
 
-class Halo {
-	public:
-		Halo();
-		double getParticleMass() const {
-			return this->particle_mass;
-		}
-		void setParticleMass(double mass) {
-			this->particle_mass = mass;
-		}
-		double getVirialRadius() const {
-			return this->virial_radius;
-		}
-		void setVirialRadius(double radius) {
-			this->virial_radius = radius;
-		}
-		double getOrigParticlePosition(int i) const {
-			return this->orig_particle_position[i];
-		}
-		void setOrigParticlePosition(int i, double value) {
-			this->orig_particle_position[i] = value;
-		}
-		void Print() const {
-			printf("Mass (%.3e) Virial Radius (%.3e) Orig Position (%.3e,%.3e,%.3e)\n", this->particle_mass, this->virial_radius, this->orig_particle_position[0], this->orig_particle_position[1], this->orig_particle_position[2]);
-		}
-	private:
-		double particle_mass;
-		double virial_radius;
-		double orig_particle_position[3];
-};
-
-Halo::Halo() {
-	this->particle_mass = 0.;
-	this->virial_radius = 0.;
-	this->orig_particle_position[0] = 0.;
-	this->orig_particle_position[1] = 0.;
-	this->orig_particle_position[2] = 0.;
-}
-
 int ReadHaloConfigFile(const std::string& filepath, libconfig::Config& config) {
 	printf("Opening file (%s)\n", filepath.c_str());
 	FILE* config_file = fopen(filepath.c_str(), "r");
@@ -63,7 +25,7 @@ int ReadHaloConfigFile(const std::string& filepath, libconfig::Config& config) {
 	return 0;
 }
 
-int GetNamedValue(const libconfig::Setting& setting, const std::string& name, double& value) {
+int CheckSpecialValueExists(const libconfig::Setting& setting, const std::string& name) {
 	if (!setting.exists(name)) {
 		printf("The setting (%s) dosen't exist!!\n", name.c_str());
 		return -1;
@@ -72,44 +34,222 @@ int GetNamedValue(const libconfig::Setting& setting, const std::string& name, do
 		printf("The setting (%s) doesn't have a value!!\n", name.c_str());
 		return -1;
 	}
-	value = setting[name.c_str()]["value"];
 	return 0;
 }
 
-int GetHaloList(const libconfig::Config& config, std::vector<Halo>& halo_list) {
-	halo_list.clear();
-	if (! config.exists("halos")) {
-		printf("Halo List doesn't exist!!\n");
+template<class T>
+int GetSpecialNamedValue(const libconfig::Setting& setting, const std::string& name, T& value) {
+	if(CheckSpecialValueExists(setting, name) < 0) {
 		return -1;
 	}
-	libconfig::Setting& halos = config.lookup("halos");
+	try {
+		value = setting[name.c_str()].lookup("value");
+		return 0;
+	} catch (libconfig::SettingTypeException e) {
+		printf("There was a setting type exception for setting (%s)!\n", e.getPath());
+		return -2;
+	}
+}
+
+template<>
+int GetSpecialNamedValue(const libconfig::Setting& setting, const std::string& name, std::string& value) {
+	if(CheckSpecialValueExists(setting, name) < 0) {
+		return -1;
+	}
+	try {
+		value = setting[name.c_str()].lookup("value").c_str();
+		return 0;
+	} catch (libconfig::SettingTypeException e) {
+		printf("There was a setting type exception for setting (%s)!\n", e.getPath());
+		return -2;
+	}
+}
+
+int CheckValueExists(const libconfig::Setting& setting, const std::string& name) {
+	if (!setting.exists(name)) {
+		printf("The setting (%s) dosen't exist!!\n", name.c_str());
+		return -1;
+	}
+	return 0;
+}
+
+template<class T>
+int GetNamedValue(const libconfig::Setting& setting, const std::string& name, T& value) {
+	if(CheckValueExists(setting, name) < 0) {
+		return -1;
+	}
+	try {
+		value = setting.lookup(name);
+		return 0;
+	} catch (libconfig::SettingTypeException e) {
+		printf("There was a setting type exception for setting (%s)!\n", e.getPath());
+		return -2;
+	}
+}
+
+template<>
+int GetNamedValue(const libconfig::Setting& setting, const std::string& name, std::string& value) {
+	if(CheckValueExists(setting, name) < 0) {
+		return -1;
+	}
+	try {
+		value = setting.lookup(name).c_str();
+		return 0;
+	} catch (libconfig::SettingTypeException e) {
+		printf("There was a setting type exception for setting (%s)!\n", e.getPath());
+		return -2;
+	}
+}
+
+class Halo {
+	public:
+		Halo();
+		int getParticleIdentifier() const {
+			return this->particle_identifier;
+		}
+		void setParticleIdentifier(int id) {
+			this->particle_identifier = id;
+		}
+		double getParticleMass() const {
+			return this->particle_mass;
+		}
+		void setParticleMass(double mass) {
+			this->particle_mass = mass;
+		}
+		double getVirialRadius() const {
+			return this->virial_radius;
+		}
+		void setVirialRadius(double radius) {
+			this->virial_radius = radius;
+		}
+		double getOrigParticlePosition(int i) const {
+			return this->orig_particle_position[i];
+		}
+		void setOrigParticlePosition(int i, double value) {
+			this->orig_particle_position[i] = value;
+		}
+		void Print() const {
+			printf("Particle (%i) Mass (%.3e) Virial Radius (%.3e) Orig Position (%.3e,%.3e,%.3e)\n", this->particle_identifier, this->particle_mass, this->virial_radius, this->orig_particle_position[0], this->orig_particle_position[1], this->orig_particle_position[2]);
+		}
+		int setFromSetting(const libconfig::Setting& halo_setting);
+	private:
+		int particle_identifier;
+		double particle_mass;
+		double virial_radius;
+		double orig_particle_position[3];
+};
+
+Halo::Halo() {
+	this->particle_mass = 0.;
+	this->virial_radius = 0.;
+	this->orig_particle_position[0] = 0.;
+	this->orig_particle_position[1] = 0.;
+	this->orig_particle_position[2] = 0.;
+}
+
+int Halo::setFromSetting(const libconfig::Setting& halo_setting) {
+	int identifier;
+	if(GetSpecialNamedValue(halo_setting, "particle_identifier", identifier) < 0) {
+		return -1;
+	}
+	double particle_mass;
+	if(GetSpecialNamedValue(halo_setting, "particle_mass", particle_mass) < 0) {
+		return -1;
+	}
+	double virial_radius;
+	if(GetSpecialNamedValue(halo_setting, "virial_radius", virial_radius) < 0) {
+		return -1;
+	}
+	double orig_pos_x;
+	if(GetSpecialNamedValue(halo_setting, "orig_particle_position_x", orig_pos_x) < 0) {
+		return -1;
+	}
+	double orig_pos_y;
+	if(GetSpecialNamedValue(halo_setting, "orig_particle_position_y", orig_pos_y) < 0) {
+		return -1;
+	}
+	double orig_pos_z;
+	if(GetSpecialNamedValue(halo_setting, "orig_particle_position_z", orig_pos_z) < 0) {
+		return -1;
+	}
+
+	this->setParticleIdentifier(identifier);
+	this->setParticleMass(particle_mass);
+	this->setVirialRadius(virial_radius);
+	this->setOrigParticlePosition(0, orig_pos_x);
+	this->setOrigParticlePosition(1, orig_pos_y);
+	this->setOrigParticlePosition(2, orig_pos_z);
+	return 0;
+}
+
+class SimHalos {
+	public:
+		SimHalos();
+		std::vector<Halo>& getHalos() {
+			return this->halos;
+		}
+		void setHalos(std::vector<Halo>&& halos) {
+			this->halos = halos;
+		}
+		size_t size() const {
+			return this->halos.size();
+		}
+		int getSimNum() const {
+			return this->sim_num;
+		}
+		void setSimNum(int sim_num) {
+			this->sim_num = sim_num;
+		}
+		const std::string& getTimeSlice() const {
+			return this->time_slice;
+		}
+		void setTimeSlice(const std::string& time_slice) {
+			this->time_slice = time_slice;
+		}
+
+		int setFromSetting(libconfig::Setting& sim_setting);
+
+
+	private:
+		std::vector<Halo> halos;
+		int sim_num;
+		std::string time_slice;
+};
+
+SimHalos::SimHalos() {
+	sim_num = -1;
+	time_slice = "";
+}
+
+int SimHalos::setFromSetting(libconfig::Setting& sim_setting) {
+	int sim_num;
+	if (GetNamedValue(sim_setting, "sim_num", sim_num) < 0) {
+		return -1;
+	}
+	std::string time_slice;
+	if (GetNamedValue(sim_setting, "time_slice", time_slice) < 0) {
+		return -1;
+	}
+	if(! sim_setting.exists("halos")) {
+		return -1;
+	}
+
+	std::vector<Halo> halo_list;
+	libconfig::Setting& halos = sim_setting.lookup("halos");
 	halo_list.reserve(halos.getLength());
 	for(int i=0; i< halos.getLength(); ++i) {
 		libconfig::Setting& halo = halos[i];
 		Halo new_halo;
-		double temp;
-		if(GetNamedValue(halo, "particle_mass", temp) < 0) {
-			return -2;
+		if(new_halo.setFromSetting(halo) < 0) {
+			printf("Warning! Couldn't get halo from settings!\n");
+		} else {
+			halo_list.push_back(std::move(new_halo));
 		}
-		new_halo.setParticleMass(temp);
-		if(GetNamedValue(halo, "virial_radius", temp) < 0) {
-			return -2;
-		}
-		new_halo.setVirialRadius(temp);
-		if(GetNamedValue(halo, "orig_particle_position_x", temp) < 0) {
-			return -2;
-		}
-		new_halo.setOrigParticlePosition(0, temp);
-		if(GetNamedValue(halo, "orig_particle_position_y", temp) < 0) {
-			return -2;
-		}
-		new_halo.setOrigParticlePosition(1, temp);
-		if(GetNamedValue(halo, "orig_particle_position_z", temp) < 0) {
-			return -2;
-		}
-		new_halo.setOrigParticlePosition(2, temp);
-		halo_list.push_back(new_halo);
 	}
+
+	this->setSimNum(sim_num);
+	this->setTimeSlice(time_slice);
+	this->setHalos(std::move(halo_list));
 	return 0;
 }
 
@@ -126,6 +266,7 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	std::vector<SimHalos> SimGroup;
 	for(auto filepaths_i = sim_halo_data_filepaths.begin(); filepaths_i < sim_halo_data_filepaths.end(); ++filepaths_i) {
 		std::string filepath = *filepaths_i;
 		libconfig::Config config;
@@ -133,14 +274,13 @@ int main(int argc, char** argv) {
 			printf("There was a problem reading the halo file!\n");
 			continue;
 		}
-		std::vector<Halo> HaloList;
-		GetHaloList(config, HaloList);
-
-		printf("Found the halos: \n");
-		for(auto halo_i = HaloList.begin(); halo_i < HaloList.end(); ++halo_i) {
-			Halo& halo = *halo_i;
-			halo.Print();
+		SimHalos sim_halos;
+		if(sim_halos.setFromSetting(config.getRoot())) {
+			printf("There was a problem setting the sim object from the Settings Object\n");
+			continue;
 		}
+		printf("Found sim with %lu halos.\n", sim_halos.size());
+		SimGroup.push_back(sim_halos);
 	}
 
 	return 0;
